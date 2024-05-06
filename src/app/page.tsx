7339125ee20c12dcbc7ca027fc50b81448fd924f
kwani6684 <jack0910@naver.com>
2024-05-06 15:32:55 +0900

    style: framer-motion 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 export default function Home() {
   return (
     <div className="flex-col justify-center">
       <div className="flex justify-center">독서관리 타이머 앱</div>
       {/* 벤또 그리드 */}
-      <div className="w-full px-4 py-10 bg-cyan-900">
-        <div className="grid gap-x-2 gap-y-2 w-full grid-cols-2">
-          <div className="flex justify-center items-center bg-pink-300 h-[300px] rounded-lg col-span-2">
+      <div className="w-full px-6 py-10 bg-cyan-900">
+        <div className="grid gap-x-8 gap-y-8 w-full grid-cols-2">
+          <motion.div
+            whileHover={{ scale: 1.1, transition: { duration: 1 } }}
+            className="flex justify-center items-center bg-pink-300 h-[300px] rounded-lg col-span-2"
+          >
             독서 타이머
-          </div>
+          </motion.div>
           <div className="flex justify-center h-[150px] items-center bg-sky-400 rounded-lg">
             내 기록
           </div>
