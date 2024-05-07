7b365a53ef8bb0f633fc4557da5f14138b86f483
kwani6684 <jack0910@naver.com>
2024-05-08 01:10:36 +0900

    chore: timeSelect 디렉토리 변경

--- src/app/(pages)/timer/page.tsx
+++ src/app/(pages)/timer/page.tsx
@@ -1,9 +1,11 @@
-import TimerSelect from '@/app/components/timerSelect'
+import TimerCircle from '@/app/components/timer/timerCircle'
+import TimerSelect from '@/app/components/timer/timerSelect'
 
 const Timer = () => {
   return (
     <div className="grid px-4 gap-x-2 py-10 h-[400px] grid-cols-4">
       <TimerSelect />
+      <TimerCircle />
     </div>
   )
 }
