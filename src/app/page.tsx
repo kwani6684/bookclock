84bcd21200f45c6383f96fbd41b6e3978db0bc25
kwani6684 <jack0910@naver.com>
2024-05-07 10:56:55 +0900

    chore: 메인 페이지 주석 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Bento from './components/bento'
 
 export default function Home() {
@@ -7,16 +8,18 @@ export default function Home() {
       {/* 벤또 그리드 */}
       <div className="w-full px-6 py-10 bg-cyan-900">
         <div className="grid gap-x-8 gap-y-8 w-full grid-cols-2">
-          <Bento
-            content="독서 타이머"
-            color="bg-pink-400"
-            height={300}
-            size="2"
-          />
-
-          <Bento content="내 기록" color="bg-sky-400" height={150} size="1" />
-
-          <Bento content="랭킹" color="bg-emerald-400" height={150} size="1" />
+          {/* 타이머 */}
+          <Link className="col-span-2" href={'/timer'}>
+            <Bento content="독서 타이머" color="bg-pink-400" height={300} />
+          </Link>
+          {/* 내 기록  */}
+          <Link className="col-span-1" href={'/logs'}>
+            <Bento content="내 기록" color="bg-sky-400" height={150} />
+          </Link>
+          {/* 랭킹 */}
+          <Link className="col-span-1" href={'/ranking'}>
+            <Bento content="랭킹" color="bg-emerald-400" height={150} />
+          </Link>
         </div>
       </div>
     </div>
