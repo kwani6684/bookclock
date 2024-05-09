8efe4a3ee34b8e9bdad71683b227de19877313ed
kwani6684 <jack0910@naver.com>
2024-05-09 17:02:58 +0900

    Design: isOnTimer에 따른 애니메이션 추가

--- src/app/(pages)/timer/page.tsx
+++ src/app/(pages)/timer/page.tsx
@@ -5,19 +5,57 @@ import TimerCircle from '@/app/components/timer/timerCircle'
 import TimerSelect from '@/app/components/timer/timerSelect'
 import TimerStart from '@/app/components/timer/timerStart'
 import { RootState } from '@/redux/store'
+import { motion } from 'framer-motion'
 import { useSelector } from 'react-redux'
 
 const Timer = () => {
   const isWhatTimer = useSelector((state: RootState) => state.timerReducer)
-
+  const isOntimer = useSelector((state: RootState) => state.onTimerReducer)
   return (
     <div>
-      <TimerSelect />
-      <div className="flex justify-center ">
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: isOntimer ? 0 : 1, y: isOntimer ? -200 : 0 }}
+        transition={{ delay: 0.3, duration: 0.5, type: 'spring' }}
+      >
+        <TimerSelect />
+      </motion.div>
+      <div>
         {isWhatTimer !== 0 &&
-          (isWhatTimer === -1 ? <TimerCircle /> : <InfinityTimer />)}
+          (isWhatTimer === -1 ? (
+            <div>
+              <motion.div
+                animate={{
+                  y: isOntimer ? -200 : 0,
+                }}
+                transition={{ delay: 0.3, duration: 0.5, type: 'spring' }}
+              >
+                <div className="flex justify-center">
+                  <TimerCircle />
+                </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 1 }}
+                animate={{
+                  opacity: isOntimer ? 0 : 1,
+                }}
+              >
+                <div className="flex justify-center ">
+                  <TimerStart />
+                </div>
+              </motion.div>
+            </div>
+          ) : (
+            <motion.div
+              animate={{
+                y: isOntimer ? -200 : 0,
+              }}
+              transition={{ delay: 0.3, duration: 0.5, type: 'spring' }}
+            >
+              <InfinityTimer />
+            </motion.div>
+          ))}
       </div>
-      <TimerStart />
     </div>
   )
 }
