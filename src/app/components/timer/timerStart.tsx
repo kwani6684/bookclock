45ed63b74109e56da44ea1cec9fbb01dc682555c
kwani6684 <jack0910@naver.com>
2024-05-10 11:24:14 +0900

    Feat: timerStart 컴포넌트 추가

--- src/app/components/timer/timerStart.tsx
+++ src/app/components/timer/timerStart.tsx
@@ -1,36 +1,97 @@
-import { motion } from 'framer-motion'
-import { AppDispatch, RootState } from '@/redux/store'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setAngle } from '@/redux/features/angleSlice'
+import { RootState } from '@/redux/store'
+import { setOnTimer } from '@/redux/features/onTimerSlice'
+import CustomAlert from '../alert'
+import StartPageInput from '../log/startPageInput'
 
 const TimerStart = () => {
-  const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch()
   const angle = useSelector((state: RootState) => state.angleReducer)
+  const [showInput, setShowInput] = useState(false)
+  const [showAlert, setShowAlert] = useState(false)
+
   const handleAngle = (a: number) => {
     dispatch(setAngle(a))
   }
+  const handleTimerStart = () => {
+    dispatch(setOnTimer())
+  }
+
+  const closeAlert = () => {
+    setShowAlert(false)
+  }
+
+  const handleNext = () => {
+    if (showInput) {
+      setShowAlert(true)
+    } else {
+      setShowInput(true)
+    }
+  }
+  const handlePrev = () => {
+    setShowInput(false)
+  }
+
   return (
-    <div className="flex-col py-10">
-      <motion.div animate={{ x: -100 }} className="flex overflow-x-hidden">
-        <div className="flex justify-center px-1 ">
-          {[45, 90, 180].map((selectedAngle, i) => {
-            return (
-              <button
-                key={i}
-                onClick={() => handleAngle(selectedAngle)}
-                className={`main-button-container mx-1 ${angle.angle === selectedAngle ? 'bg-blue-400' : ''}`}
-              >
-                {selectedAngle / 3}m
-              </button>
-            )
-          })}
+    <div>
+      {showAlert && (
+        <CustomAlert
+          message={'독서를 시작할까요?'}
+          onClose={closeAlert}
+          isActive={true}
+          active={handleTimerStart}
+        />
+      )}
+
+      <div className="flex-col pt-6 max-h-[80px]">
+        <AnimatePresence>
+          {!showInput && (
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              //   exit={{ opacity: 0, x: -100 }}
+              className="flex overflow-x-hidden"
+            >
+              <div className="flex justify-center px-1 z-30 ">
+                {[3, 90, 180].map((selectedAngle, i) => (
+                  <button
+                    key={i}
+                    onClick={() => handleAngle(selectedAngle)}
+                    className={`main-button-container mx-1 ${angle.angle === selectedAngle ? 'bg-blue-400' : ''}`}
+                  >
+                    {selectedAngle / 3}m
+                  </button>
+                ))}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        {showInput && (
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.4, type: 'spring' }}
+            // transition={{ delay: 0.5 }}
+          >
+            <StartPageInput />
+          </motion.div>
+        )}
+      </div>
+      <div className={`flex ${showInput ? `justify-between` : `justify-end`}`}>
+        {showInput && (
+          <div className="flex pt-6 justify-start">
+            <button onClick={handlePrev}>이전</button>
+          </div>
+        )}
+        <div className="flex pt-6 justify-end">
+          <button onClick={handleNext}>다음</button>
         </div>
-        <div>sfdddaddd</div>
-      </motion.div>
-      <div className="flex py-6 justify-end">
-        <button>다음</button>
       </div>
     </div>
   )
 }
+
 export default TimerStart
