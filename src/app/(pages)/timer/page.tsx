8f762f27583369c3655791692cb53d87e13ae8b4
kwani6684 <jack0910@naver.com>
2024-05-10 11:22:07 +0900

    Refactor: InfinityTimer->plusTimer 명칭 및 기능 분리

--- src/app/(pages)/timer/page.tsx
+++ src/app/(pages)/timer/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import InfinityTimer from '@/app/components/timer/InfinityTimer'
+import PlusTimer from '@/app/components/timer/plusTimer'
 import TimerCircle from '@/app/components/timer/timerCircle'
 import TimerSelect from '@/app/components/timer/timerSelect'
 import TimerStart from '@/app/components/timer/timerStart'
@@ -11,8 +11,12 @@ import { useSelector } from 'react-redux'
 const Timer = () => {
   const isWhatTimer = useSelector((state: RootState) => state.timerReducer)
   const isOntimer = useSelector((state: RootState) => state.onTimerReducer)
+  const isCompleteTimer = useSelector(
+    (state: RootState) => state.completeTimerReducer,
+  )
   return (
-    <div>
+    <div className="relative h-[100vh]">
+      {/* {!isCompleteTimer && ( */}
       <motion.div
         initial={{ opacity: 1 }}
         animate={{ opacity: isOntimer ? 0 : 1, y: isOntimer ? -200 : 0 }}
@@ -20,6 +24,8 @@ const Timer = () => {
       >
         <TimerSelect />
       </motion.div>
+      {/* )} */}
+
       <div>
         {isWhatTimer !== 0 &&
           (isWhatTimer === -1 ? (
@@ -52,10 +58,24 @@ const Timer = () => {
               }}
               transition={{ delay: 0.3, duration: 0.5, type: 'spring' }}
             >
-              <InfinityTimer />
+              <PlusTimer />
             </motion.div>
           ))}
       </div>
+      <motion.div
+        className=""
+        initial={{ y: 0, opacity: 0 }}
+        animate={{
+          opacity: isCompleteTimer ? 1 : 0,
+          y: isCompleteTimer ? -300 : 0,
+        }}
+        transition={{ delay: 0.3, duration: 0.5, type: 'spring' }}
+      >
+        <div className="flex justify-center">
+          <div>독서를 끝마쳤어요</div>
+          <div></div>
+        </div>
+      </motion.div>
     </div>
   )
 }
--- src/app/components/timer/plusTimer.tsx
+++ src/app/components/timer/plusTimer.tsx
@@ -1,18 +1,49 @@
 import React, { useState } from 'react'
 import moment from 'moment'
+import { motion } from 'framer-motion'
+
 import { setOffTimer, setOnTimer } from '@/redux/features/onTimerSlice'
+import { setCompleteTimer } from '@/redux/features/completeTimerSlice'
+import confetti from 'canvas-confetti'
 import { AppDispatch } from '@/redux/store'
 import { useDispatch } from 'react-redux'
+import CustomAlert from '../alert'
+import StartPageInput from '../log/startPageInput'
 
 const PlusTimer = () => {
   const [time, setTime] = useState(moment.duration(0)) // duration(시작시간 1000=1초)
   const [timeTick, setTimeTick] = useState<NodeJS.Timeout | null>(null)
   const dispatch = useDispatch<AppDispatch>()
+  const [showAlert, setShowAlert] = useState(false)
+  const [showCompleteAlert, setShowCompleteAlert] = useState(false)
 
+  const [showInput, setShowInput] = useState(true)
+  const [isPause, setIsPause] = useState(true)
   const handleOffTimer = () => {
     dispatch(setOffTimer())
   }
+  const handleCompleteTimer = () => {
+    dispatch(setCompleteTimer())
+    handleOffTimer()
+
+    confetti({
+      particleCount: 100, // 파티클 개수
+      spread: 70, // 퍼지는 각도
+      origin: { y: 0.6 }, // 시작 위치 조정
+    })
+  }
+  const handleAlert = () => {
+    setShowAlert(true)
+  }
+  const closeAlert = () => {
+    setShowAlert(false)
+  }
+  const closeCompleteAlert = () => {
+    setShowCompleteAlert(false)
+  }
+
   const handleOnTimer = () => {
+    setShowInput(false)
     dispatch(setOnTimer())
   }
   const startTimer = () => {
@@ -25,42 +56,82 @@ const PlusTimer = () => {
   }
 
   const pauseTimer = () => {
-    if (timeTick) {
-      clearInterval(timeTick)
+    if (isPause) {
+      if (timeTick) {
+        clearInterval(timeTick)
+        setIsPause(false)
+      }
+    } else {
+      startTimer()
+      setIsPause(true)
     }
   }
 
   const stopTimer = () => {
+    setIsPause(true)
     pauseTimer()
-    handleOffTimer()
-    setTime(moment.duration(0))
+    setShowCompleteAlert(true)
+    // setShowInput(true)
+    // setTime(moment.duration(0))
   }
 
   return (
     <div>
+      {showAlert && (
+        <CustomAlert
+          message={'독서를 시작할까요?'}
+          onClose={closeAlert}
+          isActive={true}
+          active={startTimer}
+        />
+      )}
+
       <div className="flex justify-center text-6xl">
         {moment(time.asSeconds(), 's').format('HH:mm:ss')}
       </div>
       <div>
-        <button
-          className="main-button-container mx-1"
-          onClick={() => startTimer()}
-        >
-          시작
-        </button>
-        <button
-          className="main-button-container mx-1"
-          onClick={() => pauseTimer()}
-        >
-          일시정지
-        </button>
-        <button
-          className="main-button-container mx-1"
-          onClick={() => stopTimer()}
-        >
-          정지
-        </button>
+        <div className="flex justify-center">
+          <button
+            className="main-button-container mx-1"
+            onClick={() => pauseTimer()}
+          >
+            {isPause ? '=' : '시작'}
+          </button>
+          <button
+            className="main-button-container mx-1"
+            onClick={() => stopTimer()}
+          >
+            완료
+          </button>
+        </div>
+        {showInput && (
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.4, type: 'spring' }}
+          >
+            <div className="flex justify-center">
+              <StartPageInput />
+            </div>
+            <div className="flex justify-center">
+              <button className="main-button-container" onClick={handleAlert}>
+                시작
+              </button>
+            </div>
+          </motion.div>
+        )}
       </div>
+      {showCompleteAlert && (
+        <div className="mt-[200px]">
+          <CustomAlert
+            message={'독서를 기록할까요?'}
+            onClose={closeCompleteAlert}
+            isActive={true}
+            yPosition={200}
+            active={handleCompleteTimer}
+          />
+        </div>
+      )}
     </div>
   )
 }
