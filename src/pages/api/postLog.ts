d6b72c059bde81202377230281920483a01cb532
kwani6684 <jack0910@naver.com>
2024-05-13 15:09:25 +0900

    Feat: mongoDB 코드 입력 및 테스트 작성

---
+++ src/pages/api/postLog.ts
@@ -0,0 +1,19 @@
+import { connectDB } from '@/lib/db/database'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function showDate(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const client = (await connectDB) as any
+  const db = client.db('bookClock')
+  request.body = JSON.parse(request.body)
+  if (request.method === 'POST') {
+    try {
+      const data = {
+        time: request.body.time,
+      }
+      await db.collection('logs').insertOne(data)
+    } catch (error) {}
+  }
+}
