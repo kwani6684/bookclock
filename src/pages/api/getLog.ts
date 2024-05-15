12621695c514252a9997c85d36317d3cf1ab1956
kwani6684 <jack0910@naver.com>
2024-05-16 03:41:19 +0900

    Feat: getLog api 추가

---
+++ src/pages/api/getLog.ts
@@ -0,0 +1,18 @@
+import { connectDB } from '@/lib/db/database'
+import { ObjectId } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+async function getLog() {
+  const client = (await connectDB) as any
+  const db = client.db('bookClock')
+  const result = await db.collection('logs').find().toArray()
+  return result.reverse()
+}
+async function getLogId(id: string) {
+  const client = (await connectDB) as any
+  const db = client.db('bookClock')
+  const result = await db.collection('logs').findOne({ _id: new ObjectId(id) })
+  return result
+}
+
+export { getLog, getLogId }
