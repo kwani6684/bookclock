3713d4513f2c275c5b2de15561497b4af663f3f6
kwani6684 <jack0910@naver.com>
2024-05-16 04:12:59 +0900

    Design: 메인화면 애니메이션 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { useDispatch } from 'react-redux'
 import { setAngle } from '@/redux/features/angleSlice'
@@ -9,11 +10,14 @@ import TimerCircle from './components/timer/timerCircle'
 
 export default function Home() {
   const dispatch = useDispatch()
+  const [play, setPlay] = useState(false)
   const handleMouseEnter = () => {
     dispatch(setAngle(180))
+    setPlay(true)
   }
   const handleMouseLeave = () => {
     dispatch(setAngle(0))
+    setPlay(false)
   }
 
   return (
@@ -22,41 +26,48 @@ export default function Home() {
       <div className="w-full px-2 py-2">
         <div className="grid gap-x-2 gap-y-2 w-full grid-cols-2">
           {/* 타이머 */}
-          <Link className="col-span-2" href={'/timer'}>
-            <Bento color="bg-semiBrown" height={300}>
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                style={{
-                  originX: 0.5,
-                  originY: 0.5,
-                }}
-                className="font-black text-mainBg text-2xl"
-              >
-                독서 타이머
-              </motion.div>
 
-              <motion.div
-                initial={{ scale: 0.6 }}
-                whileHover={{ scale: 0.75 }}
-                style={{ originX: 0.5, originY: 0.2 }}
-                onHoverStart={handleMouseEnter}
-                onHoverEnd={handleMouseLeave}
-                className="flex justify-center"
-              >
-                <TimerCircle isMain={true} />
-              </motion.div>
-            </Bento>
+          <Link className="col-span-2" href={'/timer'}>
+            <motion.div
+              onHoverStart={handleMouseEnter}
+              onHoverEnd={handleMouseLeave}
+            >
+              <Bento color="bg-semiBrown" height={300}>
+                <motion.div
+                  animate={play ? { scale: 1.2 } : { scale: 1 }}
+                  transition={{ duration: 0.5 }}
+                  style={{
+                    originX: 0,
+                    originY: 0.5,
+                  }}
+                  className="font-black text-mainBg text-2xl"
+                >
+                  독서 타이머
+                </motion.div>
+
+                <motion.div
+                  initial={{ scale: 0.6 }}
+                  animate={play ? { scale: 0.75 } : { scale: 0.6 }}
+                  transition={{ duration: 0.5 }}
+                  style={{ originX: 0.5, originY: 0.2 }}
+                  className="flex justify-center"
+                >
+                  <TimerCircle isMain={true} />
+                </motion.div>
+              </Bento>
+            </motion.div>
           </Link>
+
           {/* 내 기록  */}
           <Link className="col-span-1" href={'/logs'}>
             <Bento color="bg-smallBento-primary" height={150}>
-              <div className="flex">기록</div>
+              <div className="flex font-black text-mainBg text-xl">기록</div>
             </Bento>
           </Link>
           {/* 랭킹 */}
           <Link className="col-span-1" href={'/ranking'}>
             <Bento color="bg-smallBento-primary" height={150}>
-              <div className="flex">기록</div>
+              <div className="flex font-black text-mainBg text-xl">랭킹</div>
             </Bento>
           </Link>
         </div>
