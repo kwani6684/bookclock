dd40e8570dd8c407a5ab71ed36b438b379ac7d4c
kwani6684 <jack0910@naver.com>
2024-05-17 17:03:53 +0900

    Fix: navBar 네비게이션 수정

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -9,33 +9,60 @@ import Link from 'next/link'
 import { useState } from 'react'
 import Image from 'next/image'
 import { usePathname, useRouter } from 'next/navigation'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/redux/store'
+import { setOffTimer } from '@/redux/features/onTimerSlice'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import navLogo from '../../../public/icon/nav-logo.png'
 import DropDown from './dropDown'
+import CustomAlert from './alert'
 
 const NavBar = () => {
-  const [isOpen, setisOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+  const [isAlertOpen, setIsAlertOpen] = useState(false)
   const session: any = useSession()
+  const isOnTimer = useSelector((state: RootState) => state.onTimerReducer)
+
   // const [isOpen, setIsOpen] = useState(false)
   const path = usePathname()
   const router = useRouter()
+  const dispatch = useDispatch()
+
+  const handleBack = () => {
+    router.back()
+    dispatch(setOffTimer())
+  }
+  const handleAlert = () => {
+    setIsAlertOpen(!isAlertOpen)
+  }
 
   return (
     <div className="flex items-center">
+      {isAlertOpen && (
+        <CustomAlert
+          message={`타이머가 리셋됩니다. 그래도 괜찮으시겠습니까?`}
+          onClose={handleAlert}
+          isActive={true}
+          active={() => {
+            dispatch(setOffTimer())
+            router.push('/')
+          }}
+        ></CustomAlert>
+      )}
       <div className="relative flex items-center w-full h-[60px] px-3 justify-between text-2xl ">
         {path === '/' ? (
           <Image src={navLogo} width={50} height={50} alt="로고" />
         ) : (
           <button
             className="flex justify-center text-smallBento-selected "
-            onClick={() => router.back()}
+            onClick={isOnTimer ? handleAlert : handleBack}
           >
             <ArrowBackIosIcon />
           </button>
         )}
         {path !== '/' && (
           <Link
-            className="absolute flex justify-center left-0 right-0"
+            className="absolute left-1/2 w-[50px] flex text-center transform -translate-x-1/2 right-0"
             href={'/'}
           >
             <Image src={navLogo} width={50} height={50} alt="로고" />
@@ -45,13 +72,13 @@ const NavBar = () => {
           <div className="flex flex-col items-center justify-center ">
             <button
               onClick={() => {
-                setisOpen(!isOpen)
+                setIsOpen(!isOpen)
               }}
             >
               <img
                 src={session.data.user.image}
                 alt="로고"
-                className="relative rounded-full h-[50px] w-[50px] border-smallBento-selected border-2 hover:opacity-80 z-50"
+                className="relative rounded-full h-[40px] w-[40px] border-smallBento-selected border-2 hover:opacity-80 z-50"
               />
             </button>
             <DropDown isOpen={isOpen} />
